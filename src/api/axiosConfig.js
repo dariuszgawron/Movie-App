6a16ca52de8acd9f7dfc2b826a71d18baee45191
Dariusz Gawron <screbsy+g1@gmail.com>
2022-07-15 22:27:07 +0200

    Preparation of configuration for api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,3 +1,24 @@
 import axios from 'axios';
+import queryString from 'query-string';
 
 import tmdbConfig from './tmdbConfig';
+
+const axiosConfig = axios.create({
+    baseURL: tmdbConfig.baseApiUrl,
+    paramsSerializer: params => queryString.stringify({...params, api_key: tmdbConfig.apiKey})
+});
+
+axiosConfig.interceptors.request.use(config => config);
+
+axiosConfig.interceptors.response.use(
+response => {
+    if(response && response.data) {
+        return response.data;
+    }
+    return response;
+},
+error => {
+    throw error;
+});
+
+export default axiosConfig;
--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -1,26 +1,62 @@
 import axiosConfig from './axiosConfig';
 
 const tmdbApi = {
-    getMovies: (category,params) => {
-
+    // MOVIES
+    getPopularMovies: (params) => {
+        return axiosConfig.get('movie/popular',params);
     },
-    getTvShows: (category,params) => {
-
+    getTopRatedMovies: (params) => {
+        return axiosConfig.get('movie/top_rated',params);
     },
-    searchMedia: () => {
-
+    getUpcomingMovies: (params) => {
+        return axiosConfig.get('movie/upcoming',params);
     },
-    getDetails: () => {
-
+    getMovieDetails: (id,params) => {
+        return axiosConfig.get(`movie/${id}`,params);
     },
-    getCredits: () => {
-
+    getMovieCredits: (id,params) => {
+        return axiosConfig.get(`movie/${id}/credits`,params);
     },
-    getImages: () => {
-        
+    getMovieImages: (id,params) => {
+        return axiosConfig.get(`movie/${id}/images`,params);
     },
-    getSimilar: () => {
+    getMovieVideos: (id,params) => {
+        return axiosConfig.get(`movie/${id}/videos`,params);
+    },
+    getSimilarMovies: (id,params) => {
+        return axiosConfig.get(`movie/${id}/similar`,params);
+    },
+    searchMovies: (params) => {
+        return axiosConfig.get('search/movie',params);
+    },
 
+    // TV SHOWS
+    getPopularTvShows: (params) => {
+        return axiosConfig.get('tv/popular',params);
+    },
+    getTopRatedTvShows: (params) => {
+        return axiosConfig.get('tv/top_rated',params);
+    },
+    getOnTheAirTvShows: (params) => {
+        return axiosConfig.get('tv/on_the_air',params);
+    },
+    getTvShowDetails: (id,params) => {
+        return axiosConfig.get(`tv/${id}`,params);
+    },
+    getTvShowCredits: (id,params) => {
+        return axiosConfig.get(`tv/${id}/credits`,params);
+    },
+    getTvShowImages: (id,params) => {
+        return axiosConfig.get(`tv/${id}/images`,params);
+    },
+    getTvShowVideos: (id,params) => {
+        return axiosConfig.get(`tv/${id}/videos`,params);
+    },
+    getSimilarTvShows: (id,params) => {
+        return axiosConfig.get(`tv/${id}/similar`,params);
+    },
+    searchTvShows: (params) => {
+        return axiosConfig.get('search/tv',params);
     }
 }
 
